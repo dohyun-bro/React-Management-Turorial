47037a3f22cf1605d003562bbf164c368c864f1c
root <root@localhost.localdomain>
2021-09-16 23:58:54 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import Customer from './components/Customer'
+import './App.css';
+
+const customer = [
+  {
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '나동빈',
+  'birthday': '961222',
+  'gender': '남자',
+  'job': '대학생'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '홍길동',
+  'birthday': '960305',
+  'gender': '남자',
+  'job': '프로그램어'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '이순신',
+  'birthday': '921205',
+  'gender': '남자',
+  'job': '디자이너'
+}
+]
+
+function App() {
+  return (
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })
+      }
+    </div>
+  );
+}
+
+export default App;
